1789574f0faa156ed8ab944a7d8b9f8e8b56caf9
Michael C. Amakoh <21214@nnpcgroup.com>
2022-09-07 22:10:37 +0100

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  var_1 <- function(a) {
+    x <<- a
+    inv <<- NULL
+  }
+  var_2 <- function() x
+  inverse_1 <- function(inverse) inv <<- inverse
+  inverse_2 <- function() inv
+  list(var_1 = var_1,
+       var_2 = var_2,
+       inverse_1 = inverse_1,
+       inverse_2 = inverse_2)
 }
+
 
 
 ## Write a short comment describing this function
 
+
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inv <- x$getInverse()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  mat <- x$var_2()
+  inv <- solve(mat, ...)
+  x$inverse_1(inv)
+  inv
 }
